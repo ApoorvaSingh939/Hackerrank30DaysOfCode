e2a7530f7f8782eefd6028bab2696f83fb837999
Apoorva Singh <67651664+ApoorvaSingh939@users.noreply.github.com>
2022-02-05 00:43:14 +0530

    Update Day27.java

--- Day27.java
+++ Day27.java
@@ -1,4 +1,8 @@
- static class TestDataEmptyArray {
+//TestDataUniqueValues.get_array() returns an array of at least two elements with all unique values, so for example [1,2] is sufficient, 
+//and then, TestDataUniqueValues.get_expected_result() returns 0 as the index of minimum value in this array TestDataExactlyTwoDifferentMinimums.get_array() 
+//returns an array with at exactly two different elements with minimum value, so for example [2,1,1] is fine, and then TestDataExactlyTwoDifferentMinimums.get_expected_result()
+//returns 1 as the index of first of these minima because it is the expected result according to the statement.
+static class TestDataEmptyArray {
         public static int[] get_array() {
             // complete this function
             return new int[]{};
