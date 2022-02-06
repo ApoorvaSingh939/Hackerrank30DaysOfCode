842eca50347055077a69892b473beacf4829a2bf
Apoorva Singh <67651664+ApoorvaSingh939@users.noreply.github.com>
2022-02-06 23:10:31 +0530

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String emailRegEx = ".+@gmail\\.com$";
+        Pattern pattern = Pattern.compile(emailRegEx);
+        List<String> list = new ArrayList();
+        for (int i = 0; i < num; i++){
+            String name = scanner.next();
+            String email = scanner.next();
+            Matcher matcher = pattern.matcher(email);
+            if (matcher.find()){
+                list.add(name);
+            }
+        }
+        Collections.sort(list);
+        for (String name : list){
+            System.out.println(name);
+        }
+    }
+}
